8bbbb6491ddd2fc7249ffcfa92f4c22da354065e
Start <250226337@qq.com>
2017-07-12 19:39:39 +0800

    初始化一个网络框架

---
+++ StartRequestDemo/StartRequestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StartRequestDemo
+//
+//  Created by Start on 2017/7/12.
+//  Copyright © 2017年 Start. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
